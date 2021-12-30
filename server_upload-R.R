b4583e2e6589227449327fe0abf4f1303fb1ca96
kren <kren@zhaw.ch>
2021-12-30 08:16:17 +0100

    Update server_upload-R.R

--- server_upload-R.R
+++ server_upload-R.R
@@ -5,4 +5,4 @@ rsconnect::setAccountInfo(name='kren-data-analysis',
                           token='66FE413841F3137500506CBF6BE26CC8',
                           secret='mp+YVERneFeuVFwU/xKf/RzigL9c+UJ77sQUYN3x')
 
-rsconnect::deployApp('C:/Users/kren/OneDrive - ZHAW/LABOR/ICP_APP/APP_DEV')
+rsconnect::deployApp('/Users/nikitakrahenbuhl/Documents/GitHub/icp_oes_calculator')
